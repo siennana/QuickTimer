6b5b861f3819ac229fdceff5d581a4ff3e8089da
anonymous <anonymous>
2017-05-19 11:45:04 -0700

    Now start other activity when first button pressed

--- app/src/main/java/com/example/quicktimer/MainActivity.java
+++ app/src/main/java/com/example/quicktimer/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.quicktimer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String LENGTH = "com.example.quicktimer.LENGTH";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startTimer5(View view){
+        Intent intent = new Intent(this, TimerActivity.class);
+        int length = 5; //in minutes
+        intent.putExtra(LENGTH, length);
+        startActivity(intent);
     }
 }
